17b93b167a2835559891ebdf8ea2e3b53f45ca08
h4ppyturt1e <ryan170402@gmail.com>
2021-06-14 09:15:38 +0700

    parsed rules into workable format

--- day19.py
+++ day19.py
@@ -4,29 +4,34 @@ real = "day19.txt"
 test = "day19test.txt"
 
 
-def main():
-    rules, messages = parse_file(test)
-    parse_rules(rules)
-
 def parse_file(filename: str) -> Tuple[List[str], List[str]]:
     with open(filename) as f:
         data = [line.rstrip() for line in f.readlines()]
         rules, messages = data[:data.index("")], data[data.index(""):] # split by first empty line
         [print(x) for x in data]
-    return rules, messages 
+    print()
+    return rules, messages
 
 
 def parse_rules(rules: List[str]):
-    rules = []
-    keys = [rule for rule in rules if rule[-1] == "\""]
-    
+    rule_dict = {}
+    for i, rule in enumerate(rules):
+        if rule[-1] != "\"":
+            item = rule.split(": ")[1].strip("\"")
+        else:
+            item = rule.split(": ")[1]
+        rule_dict[i] = item
+
+    [print(i, ":", rule_dict[i]) for i in rule_dict]
+
+    # keys = [rule for rule in rules if rule[-1] == "\""]
     return
 
 
-
-
+def main():
+    rules, messages = parse_file(test)
+    parse_rules(rules)
 
 
 if __name__ == '__main__':
     main()
-    
