45f39e7698d7e20b10ee1c4688af7894eed7d05c
h4ppyturt1e <ryan170402@gmail.com>
2021-06-14 08:29:43 +0700

    start of day19

--- day19.py
+++ day19.py
@@ -1,10 +1,32 @@
-import numpy
-
+from typing import List, Tuple, Dict
 
 real = "day19.txt"
+test = "day19test.txt"
 
-with open(real) as f:
-    data = [line.rstrip() for line in f.readlines()]
-    rules, messages = data[:data.index("")], data[data.index(""):]
-    [print(x) for x in rules]
-    [print(x) for x in messages]
+
+def main():
+    rules, messages = parse_file(test)
+    parse_rules(rules)
+
+def parse_file(filename: str) -> Tuple[List[str], List[str]]:
+    with open(filename) as f:
+        data = [line.rstrip() for line in f.readlines()]
+        rules, messages = data[:data.index("")], data[data.index(""):] # split by first empty line
+        [print(x) for x in data]
+    return rules, messages 
+
+
+def parse_rules(rules: List[str]):
+    rules = []
+    keys = [rule for rule in rules if rule[-1] == "\""]
+    
+    return
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+    
