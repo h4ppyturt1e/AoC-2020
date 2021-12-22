a878bc0566ec79e682d4da3a861488efe8340caf
h4ppyturt1e <ryan170402@gmail.com>
2021-12-21 19:59:09 -0800

    Revert "Initial commit"
    This reverts commit 7dd822107d8f984af04cc754b950f51482a0acc3.

--- day19.py
+++ day19.py
@@ -8,40 +8,30 @@ def parse_file(filename: str) -> Tuple[List[str], List[str]]:
     with open(filename) as f:
         data = [line.rstrip() for line in f.readlines()]
         rules, messages = data[:data.index("")], data[data.index(""):] # split by first empty line
-        # [print(x) for x in data]
+        [print(x) for x in data]
     print()
     return rules, messages
 
 
-def parse_rules(rules: List[str]) -> Dict:
+def parse_rules(rules: List[str]):
     rule_dict = {}
     for i, rule in enumerate(rules):
-        item = rule.split(": ")[1].strip("\"")
-        if '|' in item:
-            item = item.split('|')
-            item = [phrase.split() for phrase in item]
-            rule_dict[str(i)] = item
+        if rule[-1] != "\"":
+            item = rule.split(": ")[1].strip("\"")
         else:
-            rule_dict[str(i)] = item.split()
+            item = rule.split(": ")[1]
+        rule_dict[i] = item
 
     [print(i, ":", rule_dict[i]) for i in rule_dict]
 
-    return rule_dict
-
-
-def get_format(rule_dict, key, result):
-    cur_rule = rule_dict[key]
-    for sub_rule in cur_rule:
-        if len(sub_rule) != 1:
-            for rule in sub_rule:
-
-
+    # keys = [rule for rule in rules if rule[-1] == "\""]
+    return
 
 
 def main():
     rules, messages = parse_file(test)
-    rule_dict = parse_rules(rules)
-    get_format(rule_dict, '3', "")
+    parse_rules(rules)
+
 
 if __name__ == '__main__':
     main()
